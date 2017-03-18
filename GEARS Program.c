8e527b41caf1c9754f01eb47c10bd3b6c7510793
GEARS-SENAI <gears.senai.sp@gmail.com>
2017-03-17 23:24:15 -0300

    Add files via upload

---
+++ GEARS Program.c
@@ -0,0 +1,15 @@
+http://thiagomachado.net.br/downloads/jogo_da_velha.alg
+
+char tabela [3];
+char castel [3];
+boolean jogou = falso;
+ 
+for (stat = 0;tabela < 3;++tabela)
+	if (v[stat] = 2) {
+		for (castel = 0;castel < 3;++castel)
+			if (tabela [stat,castel]= " ") && (jogou = falso) {
+				pc = tabela[stat,castel]; 
+				jogou = true;
+				
+			}
+	}
